7a64267accf6cebf057d965414cd57006f60e7fa
Raul Gavris <rg.raulgavris@gmail.com>
2022-05-13 11:50:18 +0300

    awesome error fixed

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -18,8 +18,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
-require("collision")()
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
